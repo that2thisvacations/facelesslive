a7d2c806468fce7bff676545ae4ee0209a602234
that2thisvacations <dmacsoldit@yahoo.com>
2026-08-09 19:32:33 -0700

    Host transparent Professor workshop asset

--- app/api/broadcast/destinations/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { broadcastDestinations } from "@/lib/broadcast-destinations";
-
-export async function GET() {
-  return NextResponse.json({ destinations: broadcastDestinations });
-}
